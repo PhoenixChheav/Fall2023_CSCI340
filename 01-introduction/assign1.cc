012b66a6f014fa0407fe8c0bde18d5dea24566a1
Phoenix <116984416+PhoenixChheav@users.noreply.github.com>
2024-04-16 21:19:00 -0500

    Add files via upload

---
+++ 01-introduction/assign1.cc
@@ -0,0 +1,21 @@
+/**********************************************************************************************
+ * NIU CSCI 340 Section 0003                                                          *
+ * Assignment 1                                                                               *
+ * Phoenix Chheav - Z1966108 - phoenixchh                                          *
+ *                                                                                            *
+ *   I certify that everything I am submitting is either provided by the professor for use in *
+ * the assignment, or work done by me personally. I understand that if I am caught submitting *
+ * the work of others (including StackOverflow or ChatGPT) as my own is an act of Academic    *
+ * Misconduct and will be punished as such.                                                   *
+ *                                                                                            *
+ **********************************************************************************************/
+#include <iostream>
+
+int main() {
+  using std::cout;
+  using std::endl;
+
+  cout << "Hello world" << endl;
+
+  return 0;
+}
